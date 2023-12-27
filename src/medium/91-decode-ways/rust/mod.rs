a972e8fc725db38b38c1ab59f987ac31c93b6047
Timothy Quilling <teqed@shatteredsky.net>
2023-12-26 21:25:51 -0500

    Cleanup

--- src/medium/91-decode-ways/rust/mod.rs
+++ src/medium/91-decode-ways/rust/mod.rs
@@ -1,20 +1,33 @@
 struct Solution;
 
+// impl Solution {
+//     pub fn num_decodings(s: String) -> i32 {
+//         let chars: Vec<_> = s.chars().collect();
+//         chars.iter().zip(chars.iter().skip(1)).enumerate()
+//             .fold([1, 0],
+//                 |a, (i, (c, next_c))| 
+//                 [ if *c != '0' {a[0] + a[1]} else {0},
+//                     if i>0 && (*c == '1' || 
+//                     (*c == '2' && *next_c < '7')) {a[0]} else {0}
+//                 ]
+//             ).iter().sum()
+//     }
+// }
+
 impl Solution {
     pub fn num_decodings(s: String) -> i32 {
+        let splice = s.as_bytes();
+        if splice[0] == b'0' { return 0; }
+        if s.len() == 1 { return 1; }
         let mut dp = vec![0; s.len() + 1];
         dp[0] = 1;
-        dp[1] = if s.chars().nth(0).unwrap() == '0' { 0 } else { 1 };
-        for i in 2..=s.len() {
-            let one = s[i - 1..i].parse::<i32>().unwrap();
-            let two = s[i - 2..i].parse::<i32>().unwrap();
-            if (1..=9).contains(&one) {
-                dp[i] += dp[i - 1];
-            }
-            if (10..=26).contains(&two) {
-                dp[i] += dp[i - 2];
-            }
-        }
+        dp[1] = if splice[0] == b'0' { 0 } else { 1 };
+        splice.windows(2).enumerate().for_each(|(i, window)| {
+            let one = window[1] - b'0';
+            let two = (window[0] - b'0') * 10 + one;
+            if (1..=9).contains(&one) { dp[i + 2] += dp[i + 1] }
+            if (10..=26).contains(&two) { dp[i + 2] += dp[i] }
+        });
         dp[s.len()]
     }
 }
