770cfdd859d52fdff37f74fe157c25084ffadaf2
Timothy Quilling <teqed@shatteredsky.net>
2024-01-08 00:40:50 -0500

    Add solution - 938 Range sum of BST  - Rust

---
+++ src/easy/938-range-sum-of-bst/rust/mod.rs
@@ -0,0 +1,55 @@
+#![warn(
+    clippy::complexity,
+    clippy::correctness,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::perf,
+    clippy::style,
+    clippy::suspicious,
+    clippy::unwrap_used,
+    clippy::question_mark_used
+)]
+
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+        let mut sum = 0;
+        root.map_or((), |node| {
+                let node = node.borrow();
+                if node.val >= low && node.val <= high {
+                    sum += node.val;
+                }
+                if node.val > low {
+                    sum += Self::range_sum_bst(node.left.clone(), low, high);
+                }
+                if node.val < high {
+                    sum += Self::range_sum_bst(node.right.clone(), low, high);
+                }
+            });
+        sum
+    }
+}
+
+fn main () {}
